7bc8dc88f0639ee03d8370a65c98ff28c5610997
Paul Leflon <leflon.paul@gmail.com>
2025-11-14 15:55:28 +0000

    feat: add options for production

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import viewer from './viewer.html';
 
 const server = Bun.serve({
+    port: process.env.PORT || process.env.BUN_PORT || 3000,
     routes: {
         '/': viewer,
         '/geo/:id': async (req) => {
@@ -8,6 +9,7 @@ const server = Bun.serve({
             const file = Bun.file(`southkorea-maps/kostat/2018/json/${id}.json`);
             const body = await file.text();
             return new Response(body);
+        }
     },
-    },
+    development: process.env.NODE_ENV === 'development'
 });
